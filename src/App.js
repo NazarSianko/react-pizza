3bcec91a48f51c711852055beea3b020bf09f43b
NazarSianko <nazar_senko@mail.ru>
2022-08-08 19:11:20 +0300

    fix scroll

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
 import React from 'react';
-import {useDispatch} from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { Header } from './components';
 import { Home, Cart } from './pages';
 import { Route, Routes } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
 import UpArrow from './components/UpArrow';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import pizzas from './redux/reducers/pizzas';
-import {Link, animateScroll} from 'react-scroll';
+import { Link, animateScroll } from 'react-scroll';
 import filters from './redux/reducers/filters';
-import { useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
- 
   const [pageOffset, setOffset] = useState(0);
-      const showOffset = () => {
-          setOffset(window.pageYOffset);
-         
-      }
- useEffect (() => {
-
-    console.log(pageOffset)
-  },[pageOffset]);
-  
+  const showOffset = () => {
+    setOffset(window.pageYOffset);
+  };
+  useEffect(() => {
+    document.addEventListener('scroll', showOffset);
+    console.log(pageOffset);
+  }, [pageOffset]);
 
   return (
     <div className="wrapper">
-       
-        {pageOffset >150 ? <UpArrow  onChange={()=>setOffset()}/> : ''}
-      
+      {pageOffset > 150 ? <UpArrow onChange={() => setOffset(window.pageYOffset)} /> : ''}
+
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
